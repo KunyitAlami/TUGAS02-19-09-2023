3fa119b9d66734c9cf132c969752fa0b05987c7c
KunyitAlami <ghanimdzkr3@gmail.com>
2023-09-19 18:02:16 +0800

    first commit

---
+++ TUGAS2-231081711011-GHANI_MUDZAKIR.c
@@ -0,0 +1,13 @@
+/*
+Berikan Output 
+
+Selamat Pagi,
+
+Teknologi Informasi FT ULM Selalu Di Hati
+*/
+#include <stdio.h>
+int main()
+{
+    printf("Selamat Pagi\n\nTeknologi Informasi FT ULM Selalu Di Hati");
+    return 0;
+}
---
+++ TUGAS2-231081711011-GHANI_MUDZAKIR.py
@@ -0,0 +1,11 @@
+'''
+Berikan Output 
+
+Selamat Pagi,
+
+Teknologi Informasi FT ULM Selalu Di Hati
+'''
+print("Selamat Pagi\n\nTeknologi Informasi FT ULM Selalu Di Hati")
+
+
+
